52d3110b2973e8209a05b9a107ba2650d2588ef1
NicholasSmilovic <nicholas_smilovic@hotmail.com>
2017-09-11 18:15:29 +0000

    added user agent

--- download_avatars.js
+++ download_avatars.js
@@ -4,22 +4,25 @@ var GITHUB_TOKEN = "f3845a0b910e2e288fb6d7e10ab6ca146c5c596c";
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  // console.log(requestURL);
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-  request.get(requestURL)
-    .on('err', function (){
-      throw err;
-    })
-    .on('response', function(chunks){
-      console.log("chunks: ", chunks);
-    });
+  options = {
+    'url': requestURL,
+    headers:{
+      'User-Agent': "GitHub Avatar Downloader - Student Project"
+    }
+  }
+  request.get(options)
+  .on('err', function (){
+    throw err;
+  })
+  .on('response', function(response){
+    //   console.log("chunks: ", chunks);
+    console.log("Response Status Code: ", response.statusCode)
+    console.log("Response Status Message: ", response.statusMessage)
+  });
 
 }
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
-
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
