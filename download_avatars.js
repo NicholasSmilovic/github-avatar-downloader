7e50706bcd1f1cc24c6b737895ebca15f5e00257
NicholasSmilovic <nicholas_smilovic@hotmail.com>
2017-09-11 20:31:20 +0000

    implemented commend line input and made it required

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,12 @@
 var request = require('request');
 var fs = require('fs');
 
-var args = process.argv.slice(2);
-let repoOwner = args[0];
-let repoName = args[1];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url: 'https://api.github.com/repos/jquery/jquery/contributors',
+    url: 'https://api.github.com/repos/'+repoOwner+'/'+repoName+'/contributors',
     headers: {
       'User-Agent': 'NicholasSmilovic'
     }
@@ -39,14 +36,17 @@ function downloadImageByURL(url, filePath) {
     .on('response', function(response){
       console.log("Response Status Code: ", response.statusCode);
     })
-    .pipe(fs.createWriteStream(filePath));
-
+    .pipe(fs.createWriteStream(filePath)
+  );
 }
 
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/141167?v=4", "./downloaded.jpg");
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  // console.log("Result:", result);
-  // console.log("body:", body);
-});
+var args = process.argv.slice(2);
+if (args.length < 2) {
+  console.log("termination")
+} else {
+  let repoOwner = args[0];
+  let repoName = args[1];
+  getRepoContributors(repoOwner, repoName, function(err, result) {
+    console.log("Errors:", err);
+  });
+}
