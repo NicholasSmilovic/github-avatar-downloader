dbb5f013100d7da111fd59eec6e63d8b6195baf6
NicholasSmilovic <nicholas_smilovic@hotmail.com>
2017-09-11 20:16:02 +0000

    able to download one pic

--- download_avatars.js
+++ download_avatars.js
@@ -27,8 +27,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-  console.log("body:", body);
-});
+
+function downloadImageByURL(url, filePath) {
+  // ...
+  request.get(url)
+    .on('error', function(err){
+      throw err;
+    })
+    .on('response', function(response){
+      console.log("Response Status Code: ", response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath));
+
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/141167?v=4", "./downloaded.jpg");
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+//   console.log("body:", body);
+// });
